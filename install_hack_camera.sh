9f64b70f2490fac351622296d0af3756a6f54fa3
sukuna0567 <josephharrysonmarc@gmail.com>
2025-04-05 17:57:35 -0400

    Create install_hack_camera.sh

---
+++ install_hack_camera.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo ""
+clear
+echo ""
+echo -e ""
+echo ""
+echo ""
+echo -e ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Installing Tool HACK-CAMERA \e[0m"
+echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing tool Hack Camera Please Wait....  \e[0m"
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m𝚵𝚳𝚸𝚵𝚪𝚯𝚪 𝐒𝐔𝐊𝐔𝚴𝚫\e[0m"
+echo ""
+sleep 2
+termux-setup-storage
+apt-get update -y
+apt-get upgrade -y
+apt install php -y
+apt install git -y
+apt install wget -y
+apt install curl -y
+apt install unzip -y
+clear
+echo '
+      ---------------------------------   
+    !!    Installation Successfull     !!  
+      ---------------------------------   ' | lolcat
+sleep 2
+clear
+echo
+echo -e "\e[1m \e[36m[+] This is Last Step \e[m \e[21"
+echo
+echo
+echo -e "\e[92m[+] Open New type this command:\e[93m bash hack_camera.sh \e[m "
